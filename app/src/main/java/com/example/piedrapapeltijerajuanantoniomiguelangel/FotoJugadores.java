7fac036b46fd2795570a0d64881520134b2c6a00
Migui <miguelangelmgc03@gmail.com>
2024-02-19 14:14:52 +0100

    commit

--- app/src/main/java/com/example/piedrapapeltijerajuanantoniomiguelangel/FotoJugadores.java
+++ app/src/main/java/com/example/piedrapapeltijerajuanantoniomiguelangel/FotoJugadores.java
@@ -25,7 +25,7 @@ import android.widget.Toast;
 
 public class FotoJugadores extends AppCompatActivity {
     MediaPlayer media;
-    Button play;
+    Button bJ1;
     MediaRecorder recorder;
     ImageView imagenJ1;
     ImageView imagenJ2;
@@ -40,6 +40,8 @@ public class FotoJugadores extends AppCompatActivity {
         setContentView(R.layout.activity_foto_jugadores);
         MediaController mController = new MediaController(this);
         imagenJ1 = findViewById(R.id.imageViewJ1);
+        bJ1 = findViewById(R.id.button3);
+        bJ1.setVisibility(View.INVISIBLE);
 
         launcherJ1 = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
@@ -51,21 +53,6 @@ public class FotoJugadores extends AppCompatActivity {
                             Bundle extras = data.getExtras();
                             imgBitmap = (Bitmap) extras.get("data");
                             imagenJ1.setImageBitmap(imgBitmap);
-                        }
-                    }
-                }
-        );
-
-        launcherJ2 = registerForActivityResult(
-                new ActivityResultContracts.StartActivityForResult(),
-                new ActivityResultCallback<ActivityResult>() {
-                    @Override
-                    public void onActivityResult(ActivityResult o) {
-                        if(o.getResultCode() == RESULT_OK){
-                            Intent data = o.getData();
-                            Bundle extras = data.getExtras();
-                            imgBitmap2 = (Bitmap) extras.get("data");
-                            imagenJ2.setImageBitmap(imgBitmap);
                         }
                     }
                 }
@@ -101,11 +88,7 @@ public class FotoJugadores extends AppCompatActivity {
     public void hacerFotoJ1(View view) {
         Intent tomarFoto = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         launcherJ1.launch(tomarFoto);
-    }
-
-    public void hacerFotoJ2(View view) {
-        Intent grabarVideo = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        launcherJ2.launch(grabarVideo);
+        bJ1.setVisibility(View.VISIBLE);
     }
 
     public void irBatalla(View view) {
